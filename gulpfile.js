b25f63ee0ca8f38eedab2e4ecae0fb24c85fe51c
GriNAME <griname@gmail.com>
2017-03-18 20:37:10 +0300

    Add `gulp-image` plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,52 @@
-let browserSync   = require('browser-sync'),
-    gulp          = require('gulp'),
+let browserSync = require('browser-sync'),
+		gulp = require('gulp'),
 
-    postcss       = require('gulp-postcss'),
-    autoprefixer  = require('autoprefixer'),
-    cssnano       = require('cssnano'),
-    fonts         = require('postcss-font-magician'),
+		postcss = require('gulp-postcss'),
+		autoprefixer = require('autoprefixer'),
+		cssnano = require('cssnano'),
+		fonts = require('postcss-font-magician'),
 
-    sass          = require('gulp-sass'),
-    bourbon       = require('node-bourbon'),
-    media         = require('gulp-group-css-media-queries'),
-    notify        = require('gulp-notify');
+		sass = require('gulp-sass'),
+		image = require('gulp-image'),
+		media = require('gulp-group-css-media-queries'),
+		notify = require('gulp-notify');
 
 gulp.task('browserSync', function () {
-  browserSync({
-    server: {
-      baseDir: 'assets'
-    },
-    notify: false
-  })
+	browserSync({
+		server: {
+			baseDir: 'assets'
+		},
+		notify: false
+	})
 });
 
 
 gulp.task('postcss', function () {
-  const processor = ([
-    //autoprefixer({browsers: ['last 10 version']}),
-    cssnano(),
-    fonts()
-  ]);
-  return gulp.src('./assets/sass/*.sass')
-    .pipe(sass({
-      includePaths: bourbon.includePaths
-    }).on("error", notify.onError()))
-    .pipe(media())
-    .pipe(postcss(processor))
-    .pipe(gulp.dest('./assets/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+	const processor = ([
+		//autoprefixer({browsers: ['last 10 version']}),
+		cssnano(),
+		fonts()
+	]);
+	return gulp.src('./assets/sass/*.sass')
+		.pipe(sass().on("error", notify.onError()))
+		.pipe(media())
+		.pipe(postcss(processor))
+		.pipe(gulp.dest('./assets/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
+gulp.task('image', function () {
+	return gulp.src('./assets/img/**/*')
+		.pipe(image())
+		.pipe(gulp.dest('./assets/img'))
+})
 
 gulp.task('watch', ['postcss', 'browserSync'], function () {
-  gulp.watch('assets/sass/**/*.sass', ['postcss']);
-  gulp.watch('assets/*.html', browserSync.reload);
-  gulp.watch('assets/js/**/*.js', browserSync.reload);
+	gulp.watch('assets/sass/**/*.sass', ['postcss']);
+	gulp.watch('assets/*.html', browserSync.reload);
+	gulp.watch('assets/js/**/*.js', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
